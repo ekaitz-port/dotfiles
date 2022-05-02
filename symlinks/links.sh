f6f35f210cec4b07a086fb151ceceddc56ac5b12
Eka Port <ekaitz.port@gmail.com>
2022-05-02 09:23:08 +0200

    Add -f to rm

--- symlinks/links.sh
+++ symlinks/links.sh
@@ -1,4 +1,4 @@
-rm $HOME/.gitconfig
-rm $HOME/.zshrc
+rm -f $HOME/.gitconfig
+rm -f $HOME/.zshrc
 ln -s $HOME/.dotfiles/git/.gitconfig $HOME/.gitconfig
 ln -s $HOME/.dotfiles/shell/zsh/.zshrc $HOME/.zshrc
